7f5dd6de770914f5799264fb51a6005bb7099311
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-03-12 21:29:57 +0900

    Add files via upload

---
+++ quot1.php
@@ -0,0 +1,11 @@
+<?php
+	$name="Necro";
+	echo $name;
+	echo "님 반갑습니다!";
+	echo "<br>";
+	
+	$name='Necro';
+	echo $name;
+	echo '님 반갑습니다!';
+?>
+	
---
+++ quot2.php
@@ -0,0 +1,4 @@
+<?php
+	$name="Necro";
+	echo "{$name}님 반갑습니다!";
+?>
