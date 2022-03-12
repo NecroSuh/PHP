f3ad8bda723e7af61d96f1ec2e0416c2b3b49cb5
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-03-12 21:45:34 +0900

    Add files via upload

---
+++ string.php
@@ -0,0 +1,13 @@
+<?php
+	$title="<h3>연락처</h3>";
+	$name="네크로";
+	$address="OO도 OO시 OO구";
+	$phone="010-1111-2222";
+	$email="user@codingschool.biz";
+	
+	echo $title;
+	echo "이름: $name<br>";
+	echo "주소: $address<br>";
+	echo "전화번호: $phone<br>";
+	echo "이메일: $email<br>";
+?>
