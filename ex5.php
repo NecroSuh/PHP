887fc8399af9229e15f77133238db7a34bb273f7
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-03-12 22:47:24 +0900

    Add files via upload

---
+++ ex5.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<body>
+	<table border="1">
+		<th>이름</th>
+		<th>휴대폰 번호</th>
+		<th>주소</th>
+		<th>이메일</th>
+		<tr>
+			<td>Necro</td>
+			<td>###-####-####</td>
+			<td>***************</td>
+			<td>######@#####.###</td>
+		</tr>
+	</table>
+</body>
+</html>
